db17f0da554a18355c4f215d3769cecdaa666ce9
John-Lin <linton.tw@gmail.com>
2017-12-26 11:53:56 +0800

    add test

---
+++ nodesync/sync_test.go
@@ -0,0 +1,54 @@
+package nodesync
+
+import (
+	"bitbucket.org/linkernetworks/aurora/src/config"
+	"bitbucket.org/linkernetworks/aurora/src/entity"
+	"bitbucket.org/linkernetworks/aurora/src/kubeconfig"
+	// "bitbucket.org/linkernetworks/aurora/src/logger"
+	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/kubernetes"
+	"os"
+	"testing"
+)
+
+const (
+	testingConfigPath = "../../../config/testing.json"
+)
+
+func TestNodeSync(t *testing.T) {
+	if _, ok := os.LookupEnv("TEST_K8S"); !ok {
+		t.Skip("Skip kubernetes related tests")
+		return
+	}
+
+	cf := config.Read(testingConfigPath)
+
+	config, err := kubeconfig.Load("", "")
+	assert.NoError(t, err)
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	assert.NoError(t, err)
+
+	var nodeResults []*entity.Node
+	mongoService := mongo.NewMongoService(cf.Mongo.Url)
+
+	nts := New(clientset, mongoService)
+
+	nts.Sync()
+
+Watch:
+	for {
+		context := mongoService.NewContext()
+		err = context.C(entity.NodeCollectionName).Find(nil).All(&nodeResults)
+		assert.NoError(t, err)
+		if len(nodeResults) != 0 {
+			break Watch
+		}
+
+	}
+
+	nts.Stop()
+	assert.NotEqual(t, len(nodeResults), 0, "mongodb node collection is empty")
+}
