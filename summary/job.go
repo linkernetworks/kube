8bda758e19cbe6c5ade0c64e11f73a49213b165e
John-Lin <linton.tw@gmail.com>
2018-02-01 01:06:33 +0800

    move query job gpu mumber out and add test

---
+++ summary/job.go
@@ -0,0 +1,44 @@
+package summary
+
+import (
+	"bitbucket.org/linkernetworks/aurora/src/entity"
+	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
+	"gopkg.in/mgo.v2/bson"
+	_ "log"
+	"strconv"
+)
+
+type JobGPUUsageSummary []JobGPUUsage
+
+type JobGPUUsage struct {
+	Job    *entity.Job
+	NumGPU int
+}
+
+func QueryCurrentGpuUsageByUser(context *mongo.Session, uid bson.ObjectId) (JobGPUUsageSummary, error) {
+	var usageSummary []JobGPUUsage
+	var jobs []entity.Job
+	err := context.C(entity.JobCollectionName).Find(bson.M{"CreatedBy": uid}).All(&jobs)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, job := range jobs {
+		usage := JobGPUUsage{
+			Job:    &job,
+			NumGPU: GetNumGPUGpuFromJob(job),
+		}
+		usageSummary = append(usageSummary, usage)
+	}
+	return usageSummary, nil
+}
+
+func GetNumGPUGpuFromJob(job entity.Job) int {
+	sum := 0
+	// all containers in a pod
+	for _, r := range job.Request.Training.Resources {
+		num, _ := strconv.Atoi(r.Request.GetGpu())
+		sum += num
+	}
+	return sum
+}
---
+++ summary/job_test.go
@@ -0,0 +1,49 @@
+package summary
+
+import (
+	"bitbucket.org/linkernetworks/aurora/src/entity"
+	pb "bitbucket.org/linkernetworks/aurora/src/grpc/messages"
+
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetNumGPUFromPod(t *testing.T) {
+	job := entity.Job{
+		Request: &pb.JobRequest{
+			Training: &pb.JobTraining{
+				Resources: map[string]*pb.Resource{
+					"test": {
+						Request: &pb.ResourceRequest{
+							Gpu: "10",
+						},
+					},
+				},
+			},
+		},
+	}
+	num := GetNumGPUGpuFromJob(job)
+	assert.Equal(t, 10, num)
+
+	jobWith2Containers := entity.Job{
+		Request: &pb.JobRequest{
+			Training: &pb.JobTraining{
+				Resources: map[string]*pb.Resource{
+					"test": {
+						Request: &pb.ResourceRequest{
+							Gpu: "10",
+						},
+					},
+					"test2": {
+						Request: &pb.ResourceRequest{
+							Gpu: "20",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	num = GetNumGPUGpuFromJob(jobWith2Containers)
+	assert.Equal(t, 30, num)
+}
