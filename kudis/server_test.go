c7ffe30d7aaa74438abdc0ee1d1b11fbfdf014b7
c9s <yoanlin93@gmail.com>
2018-02-06 19:36:01 +0800

    run clean up function in tests

--- kudis/server_test.go
+++ kudis/server_test.go
@@ -6,28 +6,62 @@ import (
 	"bitbucket.org/linkernetworks/aurora/src/config"
 	"bitbucket.org/linkernetworks/aurora/src/deployment"
 	redis "bitbucket.org/linkernetworks/aurora/src/service/redis"
-	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/suite"
 )
 
-func TestServer(t *testing.T) {
-	cf := config.MustRead("../../../config/testing.json")
+type ServerTestSuite struct {
+	suite.Suite
+	config config.Config
+}
+
+func (suite *ServerTestSuite) TestCleanUp() {
+	cf := suite.config
 	rds := redis.New(cf.Redis)
 	dts := deployment.LoadDeploymentTargets(cf.JobController.DeploymentTargets, rds)
 
 	server := New(rds, dts)
-	assert.NotNil(t, server)
+	suite.NotNil(server)
 
 	dt, err := server.GetDeploymentTarget("default")
-	assert.NoError(t, err)
+	suite.NoError(err)
 
-	var subscription Subscription = NewPodLogsSubscription(rds, "default", dt, "mongo-0", "mongo", 10)
-	assert.NotNil(t, subscription)
+	var subscription Subscription = NewPodLogsSubscription(rds, "default", dt, "mongo-0", "mongo-sidecar", 10)
+	suite.NotNil(subscription)
 
 	success, reason, err := server.Subscribe(subscription)
-	assert.NoError(t, err)
-	assert.True(t, success)
-	t.Logf("reason: %s", reason)
+	suite.NoError(err)
+	suite.True(success)
+	suite.T().Logf("reason: %s", reason)
+
+	err = server.CleanUp()
+	suite.NoError(err)
+}
+
+func (suite *ServerTestSuite) TestSubscribe() {
+	cf := suite.config
+	rds := redis.New(cf.Redis)
+	dts := deployment.LoadDeploymentTargets(cf.JobController.DeploymentTargets, rds)
+
+	server := New(rds, dts)
+	suite.NotNil(server)
+
+	dt, err := server.GetDeploymentTarget("default")
+	suite.NoError(err)
+
+	var subscription Subscription = NewPodLogsSubscription(rds, "default", dt, "mongo-0", "mongo", 10)
+	suite.NotNil(subscription)
+
+	success, reason, err := server.Subscribe(subscription)
+	suite.NoError(err)
+	suite.True(success)
+	suite.T().Logf("reason: %s", reason)
 
 	err = subscription.Stop()
-	assert.NoError(t, err)
+	suite.NoError(err)
+}
+
+func TestServerTestSuite(t *testing.T) {
+	cf := config.MustRead("../../../config/testing.json")
+	suite.Run(t, &ServerTestSuite{config: cf})
 }
