a3254e532597e8637175e61d78bb7d9da9dd5bbe
c9s <yoanlin93@gmail.com>
2018-02-12 12:46:06 +0800

    Implement WaitFor for waiting the phase

--- pod/podtracker/tracker.go
+++ pod/podtracker/tracker.go
@@ -34,45 +34,15 @@ func matchPodName(obj interface{}, podName string) (*v1.Pod, bool) {
 	return pod, podName == pod.ObjectMeta.Name
 }
 
+// WaitFor wait for a pod to the specific phase
 func (t *PodTracker) WaitFor(waitPhase v1.PodPhase) *sync.Cond {
 	cv := &sync.Cond{}
 	t.Track(func(pod *v1.Pod) (stop bool) {
-		logger.Infof("Tracking pod=%s phase=%s", t.podName, pod.Status.Phase)
-
-		switch pod.Status.Phase {
-		case v1.PodPending:
-			// Check all containers status in a pod. when it failed to start we should stop tracking.
-			cslist := podutil.FindWaitingContainerStatuses(pod)
-			for _, cs := range cslist {
-				// Possible values are: PodInitializing, ErrImagePull, ImagePullBackOff
-				reason := cs.State.Waiting.Reason
-				switch reason {
-				case "PodInitializing", "ContainerCreating":
-					// Skip the standard states
-					logger.Infof("Container %s state is %s", cs.ContainerID, reason)
-
-				case "ErrImageInspect",
-					"ErrImagePullBackOff",
-					"ErrImagePull",
-					"ErrImageNeverPull",
-					"RegistryUnavailable",
-					"ErrInvalidImageName":
-					logger.Errorf("Container %s is waiting. Reason=%s", cs.ContainerID, reason)
-
-					// stop tracking
-					stop = true
-					return stop
-				}
-			}
-
-		// Stop the tracker if the status is completion status.
-		// Terminating won't be catched
-		case v1.PodRunning, v1.PodFailed, v1.PodSucceeded, v1.PodUnknown:
+		logger.Infof("Wait for pod=%s phase=%s wait=%s", t.podName, pod.Status.Phase)ci, waitPhase)
+		if waitPhase == pod.Status.Phase {
+			cv.Signal()
 			stop = true
-			return stop
 		}
-
-		stop = false
 		return stop
 	})
 	return cv
