a62efa045ad13f1220e4af1ee7ceb906e2c6c982
hwchiu <hwchiu@linkernetworks.com>
2018-02-13 00:00:12 +0800

    move volumes into kubernetes/volume

---
+++ volume/pvc.go
@@ -0,0 +1,62 @@
+package volume
+
+import (
+	"bitbucket.org/linkernetworks/aurora/src/entity"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func GetKubePVC(pvc entity.PersistentVolumeClaimParameter) (*v1.PersistentVolumeClaim, error) {
+	reList := make(v1.ResourceList)
+
+	storage, err := resource.ParseQuantity(pvc.Capacity)
+	if err != nil {
+		return nil, err
+	}
+	reList[v1.ResourceName("storage")] = storage
+
+	return &v1.PersistentVolumeClaim{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "PersistentVolumeClaim",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: pvc.Name,
+			Labels: map[string]string{
+				"kind": "workspace",
+			},
+		},
+		Spec: v1.PersistentVolumeClaimSpec{
+			Resources: v1.ResourceRequirements{
+				Limits:   reList,
+				Requests: reList,
+			},
+			AccessModes:      []v1.PersistentVolumeAccessMode{v1.PersistentVolumeAccessMode(pvc.AccessMode)},
+			StorageClassName: &pvc.StorageClass,
+		},
+	}, nil
+}
+
+func CreatePVC(clientset *kubernetes.Clientset, pvc entity.PersistentVolumeClaimParameter, namespace string) error {
+	kubePVC, err := GetKubePVC(pvc)
+	if err != nil {
+		return err
+	}
+
+	_, err = clientset.CoreV1().PersistentVolumeClaims(namespace).Create(kubePVC)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetPVC(clientset *kubernetes.Clientset, name string, namespace string) (*v1.PersistentVolumeClaim, error) {
+	return clientset.CoreV1().PersistentVolumeClaims(namespace).Get(name, metav1.GetOptions{})
+}
+func DeletePVC(clientset *kubernetes.Clientset, name string, namespace string) error {
+	return clientset.CoreV1().PersistentVolumeClaims(namespace).Delete(name, &metav1.DeleteOptions{})
+
+}
---
+++ volume/pvc_test.go
@@ -0,0 +1,64 @@
+package volume
+
+import (
+	"bitbucket.org/linkernetworks/aurora/src/config"
+	"bitbucket.org/linkernetworks/aurora/src/entity"
+	k8ssvc "bitbucket.org/linkernetworks/aurora/src/service/kubernetes"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+const pvcName = "testing"
+const namespace = "default"
+
+func TestCreatePersistentVolumeClaim(t *testing.T) {
+	if _, defined := os.LookupEnv("TEST_K8S"); !defined {
+		t.SkipNow()
+		return
+	}
+	cf := config.MustRead("../../config/testing.json")
+	ksvc := k8ssvc.NewFromConfig(cf.Kubernetes)
+	clientset, err := ksvc.CreateClientset()
+	assert.NoError(t, err)
+
+	pvc := entity.PersistentVolumeClaimParameter{
+		Name:         pvcName,
+		StorageClass: "slow-one",
+		Capacity:     "1Gi",
+		AccessMode:   "ReadWriteOnce",
+	}
+
+	err = CreatePVC(clientset, pvc, namespace)
+	assert.NoError(t, err)
+}
+
+func TestGetPersistentVolumeClaim(t *testing.T) {
+	if _, defined := os.LookupEnv("TEST_K8S"); !defined {
+		t.SkipNow()
+		return
+	}
+	cf := config.MustRead("../../config/testing.json")
+	ksvc := k8ssvc.NewFromConfig(cf.Kubernetes)
+	clientset, err := ksvc.CreateClientset()
+	assert.NoError(t, err)
+
+	pvc, err := GetPVC(clientset, pvcName, namespace)
+	assert.NoError(t, err)
+	assert.NotNil(t, pvc)
+	assert.Equal(t, pvcName, pvc.Name)
+}
+
+func TestDeletePersistentVolumeClaim(t *testing.T) {
+	if _, defined := os.LookupEnv("TEST_K8S"); !defined {
+		t.SkipNow()
+		return
+	}
+	cf := config.MustRead("../../config/testing.json")
+	ksvc := k8ssvc.NewFromConfig(cf.Kubernetes)
+	clientset, err := ksvc.CreateClientset()
+	assert.NoError(t, err)
+
+	err = DeletePVC(clientset, pvcName, namespace)
+	assert.NoError(t, err)
+}
