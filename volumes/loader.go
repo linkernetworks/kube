8f6df8dd32b6c0e3db8d3d7f023297b4e9b17d8d
c9s <yoanlin93@gmail.com>
2018-03-17 21:01:45 +0800

    move volume helpers to src/kubernetes/volumes/loader.go

---
+++ volumes/loader.go
@@ -0,0 +1,38 @@
+package volumes
+
+import (
+	"bitbucket.org/linkernetworks/aurora/src/types/container"
+
+	v1 "k8s.io/api/core/v1"
+)
+
+// NewVolumes creates the kubernetes volume definition from the container
+// volume def used by the pod spec.
+func NewVolumes(defs []container.Volume) (volumes []v1.Volume) {
+	for _, def := range defs {
+		volumes = append(volumes, v1.Volume{
+			Name: def.VolumeMount.Name,
+			VolumeSource: v1.VolumeSource{
+				PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{
+					ClaimName: def.ClaimName,
+				},
+			},
+		})
+	}
+
+	return volumes
+}
+
+// NewVolumeMounts creates the kubernetes volume mount definition from the
+// container volume def, it uses the defined volumes
+func NewVolumeMounts(defs []container.Volume) (mounts []v1.VolumeMount) {
+	for _, def := range defs {
+		mounts = append(mounts, v1.VolumeMount{
+			Name:      def.VolumeMount.Name,
+			SubPath:   def.VolumeMount.SubPath,
+			MountPath: def.VolumeMount.MountPath,
+		})
+	}
+
+	return mounts
+}
