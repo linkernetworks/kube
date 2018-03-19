5e9f39f91201809002005fb1f291cd5fa527d202
c9s <yoanlin93@gmail.com>
2018-03-19 16:36:48 +0800

    volume: simplify AttachVolumesToPod

--- volume/loader.go
+++ volume/loader.go
@@ -6,7 +6,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 )
 
-func NewVolume(def container.Volume) v1.Volume {
+func NewVolume(def *container.Volume) v1.Volume {
 	return v1.Volume{
 		Name: def.VolumeMount.Name,
 		VolumeSource: v1.VolumeSource{
@@ -22,7 +22,7 @@ func NewVolume(def container.Volume) v1.Volume {
 // volume def used by the pod spec.
 func NewVolumes(defs []container.Volume) (volumes []v1.Volume) {
 	for _, def := range defs {
-		volumes = append(volumes, NewVolume(def))
+		volumes = append(volumes, NewVolume(&def))
 	}
 
 	return volumes
@@ -30,7 +30,7 @@ func NewVolumes(defs []container.Volume) (volumes []v1.Volume) {
 
 // NewVolumeMounts creates the kubernetes volume mount definition from the
 // container volume def, it uses the defined volumes
-func NewVolumeMount(def container.Volume) v1.VolumeMount {
+func NewVolumeMount(def *container.Volume) v1.VolumeMount {
 	return v1.VolumeMount{
 		Name:      def.VolumeMount.Name,
 		SubPath:   def.VolumeMount.SubPath,
@@ -42,7 +42,7 @@ func NewVolumeMount(def container.Volume) v1.VolumeMount {
 // container volume def, it uses the defined volumes
 func NewVolumeMounts(defs []container.Volume) (mounts []v1.VolumeMount) {
 	for _, def := range defs {
-		mounts = append(mounts, NewVolumeMount(def))
+		mounts = append(mounts, NewVolumeMount(&def))
 	}
 
 	return mounts
