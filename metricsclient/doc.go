1966e60c98a7e141cf1b0658269f56d735da40dc
zyfdegh <zyfdegg@gmail.com>
2018-04-11 16:03:55 +0800

    update docs

--- metricsclient/doc.go
+++ metricsclient/doc.go
@@ -1,2 +1,2 @@
-// Package metricsclient retrieves resource usage of K8s Nodes and Pods.
+// Package metricsclient retrieves resource usages of K8s Nodes, Pods and Containers.
 package metricsclient
